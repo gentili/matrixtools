6e26ea3592335c574207d5f41d68f68ac78940cf
Globnobulous <gentili@mcpnet.ca>
2016-08-24 21:03:27 -0400

    Add instructions for virtual console configuration

--- scripts/vgaconsolemtop.sh
+++ scripts/vgaconsolemtop.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+#
+# To get mtop to start up automatically on virtual console tty1
+#
+# sudo systemctl edit getty@tty1
+#
+# [Service]
+# ExecStart=
+# ExecStart=-/sbin/agetty --noclear --skip-login --login-program /[path to]/vgaconsolemtop.sh %I $TERM
 
 setterm -powersave off -powerdown 0 -blank 0
 setfont Uni3-Terminus12x6
